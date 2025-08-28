61a31d39f56d391b017258fbe41e97c212693694
DarkhanOmirbay <domirbai04@gmail.com>
2025-08-28 17:04:06 +0500

    omnidesk api

--- app/core/omnidesk/omnidesk_api.py
+++ app/core/omnidesk/omnidesk_api.py
@@ -1,5 +1,8 @@
 import httpx
 from app.core.config import settings
+import re
+import base64
+from typing import List
 
 class OmnideskAPI:
     def __init__(self):
@@ -8,16 +11,54 @@ class OmnideskAPI:
         self.auth = (settings.USER_EMAIL,settings.OMNIDESK_API_KEY)
         
         
-    def send_message(self,content:str,chat_id:str):
+    async def send_message(self,content:str,chat_id:str):
         data = {
             "message":{
                 "content":content,
                 "staff_id":self.STAFF_ID # сотрудник
             }
         }
-        response = httpx.post(auth=self.auth,url=f"{self.OMNIDESK_DOMAIN}/cases/{chat_id}/messages.json",json=data)
+        async with httpx.AsyncClient() as client:
+            response = await client.post(auth=self.auth,url=f"{self.OMNIDESK_DOMAIN}/cases/{chat_id}/messages.json",json=data)
+        # response = httpx.post(auth=self.auth,url=f"{self.OMNIDESK_DOMAIN}/cases/{chat_id}/messages.json",json=data)
         
         return response.status_code
+        
+    async def download_image(self,last_message:str):
+        urls = re.findall(r'https?://\S+',last_message)
+        for url in urls:
+            async with httpx.AsyncClient() as client:
+                result = await client.get(auth=self.auth,url=url)
+            # result = httpx.get(auth=self.auth,url=url)
+        image_data = base64.b64encode(result.content).decode(encoding="utf-8")
+        return image_data
+    
+    async def download_audio(self,last_message:str):
+        urls = re.findall(r'https?://\S+',last_message)
+        for url in urls:
+            async with httpx.AsyncClient() as client:
+                result = await client.get(auth=self.auth,url=url)
+            # result = httpx.get(auth=self.auth,url=url)
+        # audio_data = base64.b64encode(result.content).decode(encoding="utf-8")
+        # return audio_data
+        return result.content
+    async def set_labels_and_group(self,chat_id:str,labels:List[int],group:str):
+        data = {
+            "case": 
+                {
+                    "group_id":group,
+                    "add_labels" :labels
+                }
+        }
+        
+        async with httpx.AsyncClient() as client:
+            result = await client.put(
+                auth=self.auth,
+                url = f"{self.OMNIDESK_DOMAIN}/cases/{chat_id}.json",
+                json=data
+            )
+        return result.status_code
+            
         
         
 omnidesk_api = OmnideskAPI()
