417f6e13920194e748a4a662f9f4c924e88885f1
DarkhanOmirbay <domirbai04@gmail.com>
2025-08-28 17:03:15 +0500

    helper

---
+++ app/api/v1/chatbot/helper.py
@@ -0,0 +1,24 @@
+import re
+import httpx
+from app.models.models import Message
+from datetime import datetime,timezone
+
+async def get_message_type(last_message:str) -> str:
+    """
+    Классифицирует сообщение:
+    - text
+    - image
+    - audio
+    """
+    
+    urls = re.findall(r'https?://\S+',last_message)
+    
+    for url in urls:
+        if "attachment/download/chat/" in url:
+            if url.lower().endswith((".jpg", ".jpeg", ".png", ".gif", ".bmp", ".webp")):
+                return "image"
+            if url.lower().endswith((".mp3", ".mpeg", ".wav", ".ogg", ".m4a",".opus")):
+                return "audio"    
+    return "text"    
+
+
