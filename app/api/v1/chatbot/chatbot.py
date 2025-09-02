6c1c52356cb4136ff701183884da9585400ab772
DarkhanOmirbay <domirbai04@gmail.com>
2025-09-02 11:46:31 +0500

    add condition for labels_group

--- app/api/v1/chatbot/chatbot.py
+++ app/api/v1/chatbot/chatbot.py
@@ -11,6 +11,7 @@ from langchain_core.messages import HumanMessage,AIMessage
 import io
 from app.api.v1.chatbot.labels import LABELS,SUCCESS_ID,SUPPORT_ID,SYSTEM_PROMPT
 import json
+from datetime import datetime
 
 from zoneinfo import ZoneInfo
 
@@ -74,7 +75,9 @@ async def chat(chat_request:ChatRequest,session:AsyncSession = Depends(db_helper
             conversation += f"User: {msg.message}\n Created_at: {local_time.strftime('%Y-%m-%d %H:%M:%S %z')}\n"
             if msg.response:
                 conversation += f"Bot: {msg.response}\n"
-        conversation += f"User: {chat_request.last_message}\nBot:"
+        last_message_time = datetime.now()
+        local_time_last_msg = last_message_time.astimezone(ZoneInfo("Asia/Almaty"))
+        conversation += f"User: {chat_request.last_message} в {local_time_last_msg.strftime('%Y-%m-%d %H:%M:%S %z')}\nBot:"
         
         message = HumanMessage(content=conversation)
     
@@ -111,72 +114,77 @@ async def chat(chat_request:ChatRequest,session:AsyncSession = Depends(db_helper
     
     last_ten_msg = await crud.get_chat_history(session=session,chat_id=chat_request.chat_id)
     chat = await crud.get_chat_by_id(session=session,chat_id=chat_request.chat_id)
-    
-    
-    if not chat.labels_and_group:
-        prompt = f"""
-        Ты — классификатор чата.
 
-        Твоя задача:
-        1. Проанализировать последние 10 сообщений чата.
-        2. Определить список релевантных меток (labels).
-        3. Определить группу (group) по следующим правилам:
-        - Если клиент новый → вернуть "Success_ID".
-        - Если клиент взаимодействует меньше 2 месяцев → вернуть "Success_ID".
-        - Если клиент не новый и взаимодействует больше 2 месяцев → вернуть "Support_ID".
+ 
+    if len(last_ten_msg) ==10:
+        if not chat.labels_and_group:
+            prompt = f"""
+            Ты — классификатор чата.
 
-        Важно:
-        - Ответь строго в формате **валидного JSON**.
-        - Не добавляй никаких пояснений или текста вне JSON.
-        - Используй только указанные ID меток и групп.
+            Твоя задача:
+            1. Проанализировать последние 10 сообщений чата.
+            2. Определить список релевантных меток (labels).
+            3. Определить группу (group) по следующим правилам:
+            - Если клиент новый → вернуть "Success_ID".
+            - Если клиент взаимодействует меньше 2 месяцев → вернуть "Success_ID".
+            - Если клиент не новый и взаимодействует больше 2 месяцев → вернуть "Support_ID".
 
-        Сообщения чата:
-        {last_ten_msg}
+            Важно:
+            - Ответь строго в формате **валидного JSON**.
+            - Не добавляй никаких пояснений или текста вне JSON.
+            - Используй только указанные ID меток и групп.
 
-        Список доступных меток:
-        {LABELS}
+            Сообщения чата:
+            {last_ten_msg}
 
-        Список доступных групп:
-        Success_ID = {SUCCESS_ID}
-        Support_ID = {SUPPORT_ID}
+            Список доступных меток:
+            {LABELS}
 
-        Формат ответа (пример):
-        {{
-            "labels": [id_label_1, id_label_2],
-            "group": "96756"
-        }}
-        """
+            Список доступных групп:
+            Success_ID = {SUCCESS_ID}
+            Support_ID = {SUPPORT_ID}
 
-        response= await client.chat.completions.create(
-            model="gpt-4o",
-            response_format={
-                "type":"json_object"
-            },
-            messages=[
-                {"role": "system", "content": "Ты помощник-классификатор. Отвечай строго в JSON формате."},
-                {"role": "user", "content": prompt},
-            ],
-            temperature=0
-        )
-        result_labels_and_group = json.loads(response.choices[0].message.content)
-        tokens_2 = response.usage.total_tokens
-        logger.debug(F"labels and group: {result}")
-        
-        try:
-            request_set_label = await omnidesk_api.set_labels_and_group(
-                chat_id=chat_request.chat_id,
-                labels=result_labels_and_group['labels'],
-                group=result_labels_and_group['group'])
-        except Exception as e:
-            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                          detail=f"ERROR SETTING LABELS AND GROUP {str(e)}")
-        try:
-            set_true = await crud.set_labels_group(session=session,
-                chat_id = chat_request.chat_id)
-        except Exception as e:
-            raise HTTPException(status_code=
-                                status.HTTP_500_INTERNAL_SERVER_ERROR,
-                                detail = f"ERROR SET LABELS AND GROUP {str(e)}")
+            Формат ответа (пример):
+            {{
+                "labels": [id_label_1, id_label_2],
+                "group": "96756"
+            }}
+            """
+
+            response= await client.chat.completions.create(
+                model="gpt-4o",
+                response_format={
+                    "type":"json_object"
+                },
+                messages=[
+                    {"role": "system", "content": "Ты помощник-классификатор. Отвечай строго в JSON формате."},
+                    {"role": "user", "content": prompt},
+                ],
+                temperature=0
+            )
+            result_labels_and_group = json.loads(response.choices[0].message.content)
+            tokens_2 = response.usage.total_tokens
+            logger.debug(F"labels and group: {result}")
+            logger.debug(f"labels: {result_labels_and_group['labels']}, group {result_labels_and_group['group']}")
+            print(f"labels: {result_labels_and_group['labels']}, group {result_labels_and_group['group']}")
+            
+            try:
+                request_set_label = await omnidesk_api.set_labels_and_group(
+                    chat_id=chat_request.chat_id,
+                    labels=result_labels_and_group['labels'],
+                    group=result_labels_and_group['group'])
+            except Exception as e:
+                raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail=f"ERROR SETTING LABELS AND GROUP {str(e)}")
+            
+            
+            try:
+                set_true = await crud.set_labels_group(session=session,
+                    chat_id = chat_request.chat_id)
+            except Exception as e:
+                raise HTTPException(status_code=
+                                    status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                    detail = f"ERROR SET LABELS AND GROUP {str(e)}")
 
     
     return {
