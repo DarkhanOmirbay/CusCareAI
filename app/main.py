8fc5a7d57b5f85dfc08a85064b96649977ede81f
DarkhanOmirbay <domirbai04@gmail.com>
2025-09-08 11:15:11 +0500

    add life span for starting and stopping redis helper

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 from app.api.v1.api import api_router
 from app.core.config import settings
+from contextlib import asynccontextmanager
+from app.models.redis_helper import redis_helper
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    await redis_helper.start()
+    print("REDIS HELPER STARTED")
+    yield
+    await redis_helper.stop()
+    print("REDIS HELPER STOPPED")
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(api_router,prefix=settings.API_V1_STR)
     
