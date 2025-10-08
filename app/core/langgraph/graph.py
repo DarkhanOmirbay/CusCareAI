16bbba71df5d382f2d6ef1adfeffbeb8f3ab1f9e
DarkhanOmirbay <domirbai04@gmail.com>
2025-10-08 12:21:00 +0500

    update temp=0

--- app/core/langgraph/graph.py
+++ app/core/langgraph/graph.py
@@ -1,41 +1,36 @@
-from typing import TypedDict,Union
-from langgraph.graph import StateGraph,START,END
+from typing import TypedDict, Union
+from langgraph.graph import StateGraph, START, END
 from langchain_openai import ChatOpenAI
 from app.core.config import settings
 from app.core.logging import logger
 from langchain_core.messages import BaseMessage
-from openai import OpenAI,AsyncOpenAI
+from openai import OpenAI, AsyncOpenAI
 
 
-
-llm = ChatOpenAI(model="gpt-4o",api_key=settings.OPENAI_API_KEY)
+llm = ChatOpenAI(model="gpt-4o", api_key=settings.OPENAI_API_KEY, temperature=0)
 client = AsyncOpenAI(api_key=settings.OPENAI_API_KEY)
 
 
 class AgentState(TypedDict):
-    last_message:Union[str,BaseMessage]
-    response:str
-    system_message:Union[str,BaseMessage]
-    tokens:int
-    
-    
-    
-async def process(state:AgentState)->AgentState:
-    response = await llm.ainvoke([state["last_message"],state["system_message"]])
+    last_message: Union[str, BaseMessage]
+    response: str
+    system_message: Union[str, BaseMessage]
+    tokens: int
+
+
+async def process(state: AgentState) -> AgentState:
+    response = await llm.ainvoke([state["last_message"], state["system_message"]])
     logger.debug(f"\nAI: {response.content}")
     state["tokens"] = response.response_metadata["token_usage"]["total_tokens"]
     state["response"] = response.content
     return state
-    
-    
+
+
 graph = StateGraph(AgentState)
 
-graph.add_node("process",process)
+graph.add_node("process", process)
 
-graph.add_edge(START,"process")
-graph.add_edge("process",END)
+graph.add_edge(START, "process")
+graph.add_edge("process", END)
 
 agent = graph.compile()
-
-
-    
